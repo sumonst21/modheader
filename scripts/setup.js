9eb65cece9ea217b988ac9c8c6453488c2729175
Hao Nguyen <hao1300@gmail.com>
2022-02-26 11:08:53 -0600

    Clean up

--- scripts/setup.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-fs.copySync(
-  path.join(__dirname, '..', 'node_modules/svelte-material-ui/bare.css'),
-  path.join(__dirname, '..', 'src/svelte-material-ui.css')
-);
--- utils/env.js
+++
@@ -1,6 +0,0 @@
-// tiny wrapper with default env vars
-module.exports = {
-  NODE_ENV: (process.env.NODE_ENV || "development"),
-  BROWSER: process.env.BROWSER,
-  PORT: (process.env.PORT || 3001)
-};
