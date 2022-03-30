ded7a19c854cadd6a2d42910a6584365b2a6e927
Hao Nguyen <hao1300@gmail.com>
2022-03-30 10:06:31 -0500

    Update test snapshots

--- src/svelte/ProfileBadge.test.js
+++ src/svelte/ProfileBadge.test.js
@@ -6,9 +6,17 @@ import { render } from '@testing-library/svelte';
 expect.extend({ toMatchImageSnapshot });
 
 describe('ProfileBadge', () => {
+  beforeEach(() => {
+    document.body.style.display = 'flex';
+    document.body.style.position = 'relative';
+    document.body.style.width = '36px';
+    document.body.style.height = '36px';
+  });
+
   test('Profile badge rendered - inactive mode', async () => {
     render(ProfileBadge, {
-      profile: { shortTitle: '1', backgroundColor: '#a0a', textColor: '#000' }
+      target: document.body,
+      props: { profile: { shortTitle: '1', backgroundColor: '#a0a', textColor: '#000' } }
     });
 
     expect(await generateImage()).toMatchImageSnapshot();
@@ -16,7 +24,10 @@ describe('ProfileBadge', () => {
 
   test('Profile badge rendered - active mode', async () => {
     render(ProfileBadge, {
-      profile: { shortTitle: '1', backgroundColor: '#a0a', textColor: '#000', alwaysOn: true }
+      target: document.body,
+      props: {
+        profile: { shortTitle: '1', backgroundColor: '#a0a', textColor: '#000', alwaysOn: true }
+      }
     });
 
     expect(await generateImage()).toMatchImageSnapshot();
