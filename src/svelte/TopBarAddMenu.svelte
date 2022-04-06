006bc33c6e6da0e9aeebcb0580943e31393264cd
Hao Nguyen <hao1300@gmail.com>
2022-04-06 09:49:16 -0500

    Fix menu position

--- src/svelte/TopBarAddMenu.svelte
+++ src/svelte/TopBarAddMenu.svelte
@@ -12,44 +12,52 @@
   let addMenu;
 </script>
 
-<IconButton dense on:click={() => addMenu.setOpen(true)} title="Add" id="add-button">
-  <MdiIcon size="24" icon={mdiPlus} color={$buttonColor} />
-</IconButton>
-<Menu bind:this={addMenu} class="add-menu">
-  <List>
-    <Item
-      id="add-request-header"
-      on:SMUI:action={() =>
-        updateProfile({
-          headers: addHeader($selectedProfile.headers)
-        })}
-    >
-      Request header
-    </Item>
-    <Item
-      id="add-response-header"
-      on:SMUI:action={() =>
-        updateProfile({
-          respHeaders: addHeader($selectedProfile.respHeaders)
-        })}
-    >
-      Response header
-    </Item>
-    <Item
-      id="add-url-replacement"
-      on:SMUI:action={async () =>
-        updateProfile({
-          urlReplacements: await addUrlRedirect($selectedProfile.urlReplacements)
-        })}
-    >
-      URL redirect
-    </Item>
-    <Item
-      id="add-filter"
-      on:SMUI:action={async () =>
-        updateProfile({
-          filters: await addFilter($selectedProfile.filters)
-        })}>Filter</Item
-    >
-  </List>
-</Menu>
+<div>
+  <IconButton dense on:click={() => addMenu.setOpen(true)} title="Add" id="add-button">
+    <MdiIcon size="24" icon={mdiPlus} color={$buttonColor} />
+  </IconButton>
+  <Menu bind:this={addMenu} class="add-menu">
+    <List>
+      <Item
+        id="add-request-header"
+        on:SMUI:action={() =>
+          updateProfile({
+            headers: addHeader($selectedProfile.headers)
+          })}
+      >
+        Request header
+      </Item>
+      <Item
+        id="add-response-header"
+        on:SMUI:action={() =>
+          updateProfile({
+            respHeaders: addHeader($selectedProfile.respHeaders)
+          })}
+      >
+        Response header
+      </Item>
+      <Item
+        id="add-url-replacement"
+        on:SMUI:action={async () =>
+          updateProfile({
+            urlReplacements: await addUrlRedirect($selectedProfile.urlReplacements)
+          })}
+      >
+        URL redirect
+      </Item>
+      <Item
+        id="add-filter"
+        on:SMUI:action={async () =>
+          updateProfile({
+            filters: await addFilter($selectedProfile.filters)
+          })}>Filter</Item
+      >
+    </List>
+  </Menu>
+</div>
+
+<style module>
+  .add-menu {
+    width: 200px;
+  }
+</style>
--- src/svelte/TopBarMoreMenu.svelte
+++ src/svelte/TopBarMoreMenu.svelte
@@ -35,8 +35,7 @@
   let darkModeMenu;
 
   function openInTab() {
-    chrome.tabs.update(
-      null,
+    chrome.tabs.create(
       {
         url: chrome.runtime.getURL('app.html')
       },
@@ -66,126 +65,128 @@
   $: sendEmptyHeader = ($selectedProfile.sendEmptyHeader || false).toString();
 </script>
 
-<IconButton
-  dense
-  id="more-menu"
-  on:click={() => {
-    menu.setOpen(true);
-  }}
-  title="More"
->
-  <MdiIcon size="24" icon={mdiDotsVertical} color={$buttonColor} />
-</IconButton>
-<Menu bind:this={menu}>
-  <List>
-    <Item on:SMUI:action={() => openInTab()}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiFullscreen} color="#666" />
-      Open in tab
-    </Item>
-    <Item on:SMUI:action={() => toggleComment()}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={$selectedProfile.hideComment ? mdiCommentCheckOutline : mdiCommentRemoveOutline}
-        color="#666"
-      />
-      {$selectedProfile.hideComment ? 'Show comment' : 'Hide comment'}
-    </Item>
-    <Item
-      on:SMUI:action={() => toggleAlwaysOn()}
-      title={$selectedProfile.alwaysOn
-        ? `This profile will stay active even when it is not selected.`
-        : `This profile will only be active when selected.`}
-    >
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={$selectedProfile.alwaysOn ? mdiCheckboxMarked : mdiCheckboxBlankOutline}
-        color="#666"
-      />
-      Always stay enabled
-    </Item>
-    <Item on:SMUI:action={() => darkModeMenu.setOpen(true)} title={`Dark mode`}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiThemeLightDark} color="#666" />
-      Dark mode
-    </Item>
-    <Menu bind:this={darkModeMenu}>
-      <List>
-        <Item on:SMUI:action={() => setPreferredColorScheme(undefined)}>System default</Item>
-        <Item on:SMUI:action={() => setPreferredColorScheme('dark')}>Dark mode</Item>
-        <Item on:SMUI:action={() => setPreferredColorScheme('light')}>Light mode</Item>
-      </List>
-    </Menu>
-    <Separator nav />
-    <Item on:SMUI:action={() => removeProfile($selectedProfileIndex)}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiTrashCan} color="#666" />
-      Delete profile
-    </Item>
-    <Item on:SMUI:action={() => cloneProfile($selectedProfile)}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiContentCopy} color="#666" />
-      Clone profile
-    </Item>
-    <Item on:SMUI:action={() => showExportDialog.set(true)}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiShare} color="#666" />
-      Export / share profile(s)
-    </Item>
-    <Item on:SMUI:action={() => showImportDialog.set(true)} id="import-profile">
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiFileImportOutline} color="#666" />
-      Import profile(s)
-    </Item>
-    <Item on:SMUI:action={() => showCloudBackupDialog.set(true)}>
-      <MdiIcon class="more-menu-icon" size="24" icon={mdiCloudDownloadOutline} color="#666" />
-      Restore cloud backup
-    </Item>
-    <Separator nav />
-    <Subheader>Header override mode</Subheader>
-    <Item on:SMUI:action={() => updateProfile({ appendMode: false })}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={appendMode === 'false' ? mdiRadioboxMarked : mdiRadioboxBlank}
-        color="#666"
-      />
-      <Label>Override existing value</Label>
-    </Item>
-    <Item on:SMUI:action={() => updateProfile({ appendMode: true })}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={appendMode === 'true' ? mdiRadioboxMarked : mdiRadioboxBlank}
-        color="#666"
-      />
-      <Label>Value concatenation</Label>
-    </Item>
-    <Item on:SMUI:action={() => updateProfile({ appendMode: 'comma' })}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={appendMode === 'comma' ? mdiRadioboxMarked : mdiRadioboxBlank}
-        color="#666"
-      />
-      <Label>Comma separated</Label>
-    </Item>
+<div>
+  <IconButton
+    dense
+    id="more-menu"
+    on:click={() => {
+      menu.setOpen(true);
+    }}
+    title="More"
+  >
+    <MdiIcon size="24" icon={mdiDotsVertical} color={$buttonColor} />
+  </IconButton>
+  <Menu bind:this={menu}>
+    <List>
+      <Item on:SMUI:action={() => openInTab()}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiFullscreen} color="#666" />
+        Open in tab
+      </Item>
+      <Item on:SMUI:action={() => toggleComment()}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={$selectedProfile.hideComment ? mdiCommentCheckOutline : mdiCommentRemoveOutline}
+          color="#666"
+        />
+        {$selectedProfile.hideComment ? 'Show comment' : 'Hide comment'}
+      </Item>
+      <Item
+        on:SMUI:action={() => toggleAlwaysOn()}
+        title={$selectedProfile.alwaysOn
+          ? `This profile will stay active even when it is not selected.`
+          : `This profile will only be active when selected.`}
+      >
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={$selectedProfile.alwaysOn ? mdiCheckboxMarked : mdiCheckboxBlankOutline}
+          color="#666"
+        />
+        Always stay enabled
+      </Item>
+      <Item on:SMUI:action={() => darkModeMenu.setOpen(true)} title={`Dark mode`}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiThemeLightDark} color="#666" />
+        Dark mode
+      </Item>
+      <Menu bind:this={darkModeMenu}>
+        <List>
+          <Item on:SMUI:action={() => setPreferredColorScheme(undefined)}>System default</Item>
+          <Item on:SMUI:action={() => setPreferredColorScheme('dark')}>Dark mode</Item>
+          <Item on:SMUI:action={() => setPreferredColorScheme('light')}>Light mode</Item>
+        </List>
+      </Menu>
+      <Separator nav />
+      <Item on:SMUI:action={() => removeProfile($selectedProfileIndex)}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiTrashCan} color="#666" />
+        Delete profile
+      </Item>
+      <Item on:SMUI:action={() => cloneProfile($selectedProfile)}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiContentCopy} color="#666" />
+        Clone profile
+      </Item>
+      <Item on:SMUI:action={() => showExportDialog.set(true)}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiShare} color="#666" />
+        Export / share profile(s)
+      </Item>
+      <Item on:SMUI:action={() => showImportDialog.set(true)} id="import-profile">
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiFileImportOutline} color="#666" />
+        Import profile(s)
+      </Item>
+      <Item on:SMUI:action={() => showCloudBackupDialog.set(true)}>
+        <MdiIcon class="more-menu-icon" size="24" icon={mdiCloudDownloadOutline} color="#666" />
+        Restore cloud backup
+      </Item>
+      <Separator nav />
+      <Subheader>Header override mode</Subheader>
+      <Item on:SMUI:action={() => updateProfile({ appendMode: false })}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={appendMode === 'false' ? mdiRadioboxMarked : mdiRadioboxBlank}
+          color="#666"
+        />
+        <Label>Override existing value</Label>
+      </Item>
+      <Item on:SMUI:action={() => updateProfile({ appendMode: true })}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={appendMode === 'true' ? mdiRadioboxMarked : mdiRadioboxBlank}
+          color="#666"
+        />
+        <Label>Value concatenation</Label>
+      </Item>
+      <Item on:SMUI:action={() => updateProfile({ appendMode: 'comma' })}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={appendMode === 'comma' ? mdiRadioboxMarked : mdiRadioboxBlank}
+          color="#666"
+        />
+        <Label>Comma separated</Label>
+      </Item>
 
-    <Separator nav />
-    <Subheader>Empty header mode</Subheader>
-    <Item on:SMUI:action={() => updateProfile({ sendEmptyHeader: false })}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={sendEmptyHeader === 'false' ? mdiRadioboxMarked : mdiRadioboxBlank}
-        color="#666"
-      />
-      <Label>Remove empty header</Label>
-    </Item>
-    <Item on:SMUI:action={() => updateProfile({ sendEmptyHeader: true })}>
-      <MdiIcon
-        class="more-menu-icon"
-        size="24"
-        icon={sendEmptyHeader === 'true' ? mdiRadioboxMarked : mdiRadioboxBlank}
-        color="#666"
-      />
-      <Label>Send empty header</Label>
-    </Item>
-  </List>
-</Menu>
+      <Separator nav />
+      <Subheader>Empty header mode</Subheader>
+      <Item on:SMUI:action={() => updateProfile({ sendEmptyHeader: false })}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={sendEmptyHeader === 'false' ? mdiRadioboxMarked : mdiRadioboxBlank}
+          color="#666"
+        />
+        <Label>Remove empty header</Label>
+      </Item>
+      <Item on:SMUI:action={() => updateProfile({ sendEmptyHeader: true })}>
+        <MdiIcon
+          class="more-menu-icon"
+          size="24"
+          icon={sendEmptyHeader === 'true' ? mdiRadioboxMarked : mdiRadioboxBlank}
+          color="#666"
+        />
+        <Label>Send empty header</Label>
+      </Item>
+    </List>
+  </Menu>
+</div>
