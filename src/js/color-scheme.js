0339163b89408dca9e4dec72c52cab40307132a8
Hao Nguyen <hao1300@gmail.com>
2022-04-13 06:44:02 -0500

    Fix color scheme

--- src/js/color-scheme.js
+++ src/js/color-scheme.js
@@ -1,18 +1,56 @@
+const LIGHT_STYLE_SHEET_PARAMS = {
+  id: 'light-mode-stylesheet',
+  rel: 'stylesheet',
+  href: 'styles/smui.css'
+};
+const DARK_STYLE_SHEET_PARAMS = {
+  id: 'dark-mode-stylesheet',
+  rel: 'stylesheet',
+  href: 'styles/smui-dark.css'
+};
+
 export function setPreferredColorScheme(preferredColorScheme) {
   localStorage.preferredColorScheme = preferredColorScheme;
   reloadColorScheme();
 }
 
+function addStyleSheet(params) {
+  const link = document.createElement('link');
+  for (const [key, value] of Object.entries(params)) {
+    link[key] = value;
+  }
+  document.head.prepend(link);
+}
+
+function ensureStyleSheet(params) {
+  const styleSheet = document.getElementById(params.id);
+  if (!styleSheet) {
+    addStyleSheet(params);
+  }
+}
+
+function removeStyleSheet(params) {
+  const styleSheet = document.getElementById(params.id);
+  if (styleSheet) {
+    styleSheet.remove();
+  }
+}
+
 export function reloadColorScheme() {
   const preferredColorScheme = localStorage.preferredColorScheme;
-  const darkModeStylesheet = document.getElementById('dark-mode-stylesheet');
   if (preferredColorScheme === 'light') {
-    darkModeStylesheet.setAttribute('disabled', 'disabled');
+    removeStyleSheet(DARK_STYLE_SHEET_PARAMS);
+    ensureStyleSheet(LIGHT_STYLE_SHEET_PARAMS);
   } else if (preferredColorScheme === 'dark') {
-    darkModeStylesheet.removeAttribute('disabled');
-    darkModeStylesheet.removeAttribute('media');
+    removeStyleSheet(LIGHT_STYLE_SHEET_PARAMS);
+    ensureStyleSheet(DARK_STYLE_SHEET_PARAMS);
   } else {
-    darkModeStylesheet.removeAttribute('disabled');
-    darkModeStylesheet.setAttribute('media', 'screen and (prefers-color-scheme: dark)');
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      removeStyleSheet(LIGHT_STYLE_SHEET_PARAMS);
+      ensureStyleSheet(DARK_STYLE_SHEET_PARAMS);
+    } else {
+      removeStyleSheet(DARK_STYLE_SHEET_PARAMS);
+      ensureStyleSheet(LIGHT_STYLE_SHEET_PARAMS);
+    }
   }
 }
