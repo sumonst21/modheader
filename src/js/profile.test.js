baf3ab4346ddf3e5a5ccb7e601bf0372612ee0fc
Hao Nguyen <hao1300@gmail.com>
2022-04-19 09:58:40 -0500

    Update tests for export

--- src/js/profile.test.js
+++ src/js/profile.test.js
@@ -22,6 +22,7 @@ const {
   removeProfile,
   cloneProfile,
   sortProfiles,
+  exportProfiles,
   importProfiles,
   restoreToProfiles
 } = require('./profile.js');
@@ -225,6 +226,51 @@ describe('profile', () => {
       })
     ]);
     expect(isMutated).toEqual(true);
+  });
+
+  test('Fix profiles - restore default fields', () => {
+    const profiles = [
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        headers: [
+          { enabled: true, name: 'test name 1', value: 'test value 1', comment: 'test comment' },
+          { enabled: false, name: 'test name 2', value: 'test value 2' }
+        ],
+        headersAutocomplete: {
+          autocompleteName: ['test1', 'test2'],
+          autocompleteValue: []
+        }
+      }
+    ];
+    fixProfiles(profiles);
+    expect(profiles).toEqual([
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        backgroundColor: expect.any(String),
+        textColor: expect.any(String),
+        hideComment: true,
+        headers: [
+          { enabled: true, name: 'test name 1', value: 'test value 1', comment: 'test comment' },
+          { enabled: false, name: 'test name 2', value: 'test value 2', comment: '' }
+        ],
+        headersAutocomplete: {
+          autocompleteName: ['test1', 'test2'],
+          autocompleteValue: []
+        },
+        respHeaders: [],
+        cookieHeaders: [],
+        setCookieHeaders: [],
+        urlReplacements: [],
+        tabFilters: [],
+        urlFilters: [],
+        excludeUrlFilters: [],
+        resourceFilters: []
+      }
+    ]);
   });
 
   test('Add profile', () => {
@@ -432,6 +478,71 @@ describe('profile', () => {
         canUndo: true
       }
     );
+  });
+
+  test('Export profiles - Drop default fields', () => {
+    const profiles = [
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        backgroundColor: '#000',
+        textColor: '#fff',
+        hideComment: true,
+        headers: [
+          { enabled: true, name: 'test name 1', value: 'test value 1', comment: 'test comment' },
+          { enabled: false, name: 'test name 2', value: 'test value 2', comment: '' }
+        ],
+        headersAutocomplete: {
+          autocompleteName: ['test1', 'test2'],
+          autocompleteValue: []
+        },
+        respHeaders: [],
+        respHeadersAutocomplete: {
+          autocompleteName: [],
+          autocompleteValue: []
+        }
+      }
+    ];
+    const exportedProfiles = exportProfiles(profiles);
+    expect(JSON.parse(exportedProfiles)).toEqual([
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        headers: [
+          { enabled: true, name: 'test name 1', value: 'test value 1', comment: 'test comment' },
+          { enabled: false, name: 'test name 2', value: 'test value 2' }
+        ],
+        headersAutocomplete: {
+          autocompleteName: ['test1', 'test2'],
+          autocompleteValue: []
+        }
+      }
+    ]);
+  });
+
+  test('Export profiles - Retain styles', () => {
+    const profiles = [
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        backgroundColor: '#000',
+        textColor: '#fff',
+        hideComment: true
+      }
+    ];
+    const exportedProfiles = exportProfiles(profiles, { keepStyles: true });
+    expect(JSON.parse(exportedProfiles)).toEqual([
+      {
+        version: PROFILE_VERSION,
+        title: 'Local Profile',
+        shortTitle: '1',
+        backgroundColor: '#000',
+        textColor: '#fff'
+      }
+    ]);
   });
 
   test('Restore to profiles', () => {
